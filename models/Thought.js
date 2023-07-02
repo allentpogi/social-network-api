9f615ec6dc4db0104a8914ffdd96120bde5b4f2a
allen <allen.tuazon@gmail.com>
2023-07-02 22:42:23 +1000

    creating models

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require("mongoose");
+const Reaction = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (timestamp) {
+        return new Date(timestamp).toLocaleString();
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
